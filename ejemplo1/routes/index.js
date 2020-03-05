facc2cf517ca2a730b11919780486b113e5978ac
unknown <v25a07@gmail.com>
2020-03-05 13:42:29 -0500

    1:40

--- ejemplo1/routes/index.js
+++ ejemplo1/routes/index.js
@@ -5,20 +5,15 @@ const mu = require("../modules/MogoUtils");
 /* GET home page. */
 router.get("/", function(req, res) {
   const db = mu;
-  db.getAllDatabases(dbs => {
+  db.getAllDatabases().then(dbs => {
+    console.log("bu", dbs);
     res.render("index.ejs", { databases: dbs.databases });
   });
 });
 
 router.get("/collections/:query", (req, res) => {
-  const query = req.params.query;  
-  mu.getAllCollections(query, collections => {  
-    res.json(collections);
-  });
+  const query = req.params.query;
+  mu.getAllCollections(query).then(collections => res.json(collections));
 });
-
-
-
-
 
 module.exports = router;
