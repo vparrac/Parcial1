5e7a12ca6936eddcdb79188db6e43159c2e3f891
unknown <v25a07@gmail.com>
2020-03-07 04:49:13 -0500

    MONGO URI

--- modules/MogoUtils.js
+++ modules/MogoUtils.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://localhost:27017" || process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
 function MongoUtils() {
   mu = {};
   mu.getAllDatabases = () => {
